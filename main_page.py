13ecf1df297c8e03dbbbae822636ee509299889e
mllev92 <162049326+mllev92@users.noreply.github.com>
2024-03-12 12:19:22 +0100

    Update and rename 01 - 👋 Introduction.py to main_page.py

--- main_page.py
+++ main_page.py
@@ -8,5 +8,5 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 import pickle
 
-st.markdown("# 👋 Introduction")
-st.sidebar.markdown("# 01 - 👋 Introduction")
+st.markdown("# 👋 Bienvenue")
+st.sidebar.markdown("# 01 - 👋 Bienvenu")
