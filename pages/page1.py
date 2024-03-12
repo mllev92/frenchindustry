bb9ba4090110d2cc354a76f2b9f244deb385204e
mllev92 <162049326+mllev92@users.noreply.github.com>
2024-03-12 12:10:04 +0100

    Update and rename page1.py to 📊 DataViz.py

--- pages/page1.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-st.title("💶 French Industry 💶")
-st.sidebar.title("Sommaire")
-pages=["👋 Introduction", "📊 DataViz","⚙️ Pre-processing",'🧠 Modèles de Machine Learning',"🔮 Prédictions"]
-page=st.sidebar.radio("⬇️ Aller vers", pages)
